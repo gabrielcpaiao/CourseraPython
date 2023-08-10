a872fd8de7ea6bd70396214781b04f56cc800d5d
Gabriel Paião <costapaiao@hotmail.com>
2023-08-10 08:34:49 -0300

    Meu algoritmo numero primo

--- primo.py
+++ primo.py
@@ -4,8 +4,10 @@
 #for divisor in range(2, dividendo):
 #if dividendo % divisor == 0:
 #dividendo -= 1
+#ler -> https://rpm.org.br/cdrpm/19/4.htm#:~:text=2%2C%203%2C%205%2C%207%2C%2011%2C%2013%2C,%2C%20337%2C%20347%2C%20349%2C
 
-#dividendo = int(input("Digite um numero: "))
+dividendo = int(input(""))
+
 def maior_primo(dividendo):
   divisor = 2
   while dividendo != divisor:
@@ -17,4 +19,4 @@ def maior_primo(dividendo):
 
   return dividendo
 
-print(maior_primo(25))
+print(maior_primo(dividendo))
